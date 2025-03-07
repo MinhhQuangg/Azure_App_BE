46e88988787ec5cfbfba250031b21a53abe654b8
linhphan03 <phanthuylinh221203@gmail.com>
2025-03-06 22:38:42 -0500

    Connect db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const port = process.env.PORT || 3000;
+const db = require("./config/db.config")
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
