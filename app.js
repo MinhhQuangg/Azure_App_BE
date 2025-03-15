c0f03cb27f5ea383cdc83a36d177adb28ddee17d
linhphan03 <phanthuylinh221203@gmail.com>
2025-03-14 17:45:55 -0700

    edit config

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 const express = require('express');
 const session = require('express-session');
-const passport = require('./controllers/authController');
-const userRouter = require('./routes/userRoutes');
-const cors = require('cors');
+// const passport = require('./controllers/authController');
+// const userRouter = require('./routes/userRoutes');
+// const cors = require('cors');
 require('dotenv').config({ path: './config.env' });
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  next();
-});
+// app.use(express.urlencoded({ extended: true }));
+// app.use((req, res, next) => {
+//   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+//   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+//   next();
+// });
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('<a href="/auth/google">Login with Google</a>');
-});
+// app.get('/', (req, res) => {
+//   res.send('<a href="/auth/google">Login with Google</a>');
+// });
 
-app.use('/auth', userRouter);
+// app.use('/auth', userRouter);
 
 app.use('/chatrooms', roomRoute);
 
